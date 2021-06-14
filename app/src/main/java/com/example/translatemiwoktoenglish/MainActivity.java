f488b77f8df6345d7d94f597d2c8330ac8463663
BeginnerAdi <adityagayan@protonmail.ch>
2021-06-14 23:16:21 +0530

    Translate

---
+++ app/src/main/java/com/example/translatemiwoktoenglish/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.translatemiwoktoenglish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
